191bd71e25c334629484da0614bff7b596a43743
alexish4 <alexish4@cpp.edu>
2024-10-23 16:20:55 -0700

    Saving work almost done with pizza driver

--- Main.java
+++ Main.java
@@ -2,8 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        //Pizza p = new Pizza();
+        pizzaDriver();
+    }
+
+    public static void pizzaDriver() {
+        Pizza pizzaHut1 = new PizzaBuilder().addTopping("Pepperoni").addTopping("Mushrooms").addTopping("Sausage").setPizzaChain("Pizza Hut").createPizza();
+        Pizza pizzaHut2 = new PizzaBuilder().addTopping("Chicken").addTopping("Bacon").addTopping("Sausage").addTopping("Extra Cheese").addTopping("Pesto").addTopping("Onions").setPizzaChain("Pizza Hut").createPizza();
+        Pizza pizzaHut3 = new PizzaBuilder().addTopping("Extra Cheese").addTopping("Peppers").addTopping("Pesto").addTopping("Spicy Pork").addTopping("Tomato and Basil").addTopping("Ham and Pineapple").addTopping("Pepperoni").addTopping("Mushrooms").addTopping("Sausage").setPizzaChain("Pizza Hut").createPizza();
     }
 }
--- Pizza.java
+++ Pizza.java
@@ -10,22 +10,6 @@ public class Pizza {
     );
     //Have both constructors?
 
-//    private String pepperoni = "";
-//    private String sausage = "";
-//    private String bacon = "";
-//    private String onions = "";
-//    private String extraCheese = "";
-//    private String peppers = "";
-//    private String chicken = "";
-//    private String olives = "";
-//    private String spinach = "";
-//    private String tomatoAndBasil = "";
-//    private String beef = "";
-//    private String ham = "";
-//    private String pesto = "";
-//    private String spicyPork = "";
-//    private String hamAndPineapple = "";
-
     private List<String> selectedToppings = new ArrayList<>();
     private String size;
     private String pizzaChain;
@@ -54,31 +38,6 @@ public class Pizza {
         this.pizzaChain = pizzaChain;
     }
 
-
-//    public Pizza(String pepperoni, String sausage, String bacon, String onions, String extraCheese,
-//                 String peppers, String chicken, String olives, String spinach, String tomatoAndBasil,
-//                 String beef, String ham, String pesto, String spicyPork, String hamAndPineapple) {
-//        if(checkToAddTopping(pepperoni))
-//            this.pepperoni = pepperoni;
-//        if(checkToAddTopping(sausage))
-//            this.sausage = sausage;
-//        if(checkToAddTopping(bacon))
-//            this.bacon = bacon;
-//        if(checkToAddTopping(onions))
-//            this.onions = onions;
-//        if(checkToAddTopping(extraCheese))
-//            this.extraCheese = extraCheese;
-//        if(checkToAddTopping(peppers))
-//            this.peppers = peppers;
-//        if(checkToAddTopping(chicken))
-//            this.chicken = chicken;
-//        if(checkToAddTopping(olives))
-//            this.olives = olives;
-//        if(checkToAddTopping(spinach))
-//            this.spinach = spinach;
-//        if(checkToAddTopping(tomatoAndBasil))
-//            this.tomatoAndBasil = tomatoAndBasil;
-//    }
     public Pizza(List<String> selectedToppings, String size, String pizzaChain) {
         this.selectedToppings = selectedToppings;
         this.size = size;
@@ -91,54 +50,14 @@ public class Pizza {
         }
     }
 
-
-    public static class PizzaBuilder {
-        private static final List<String> ALL_TOPPINGS = List.of(
-                "Pepperoni", "Sausage", "Mushrooms", "Bacon", "Onions",
-                "Extra Cheese", "Peppers", "Chicken", "Olives", "Spinach",
-                "Tomato and Basil", "Beef", "Ham", "Pesto",
-                "Spicy Pork", "Ham and Pineapple"
-        );
-
-        private List<String> selectedToppings = new ArrayList<>();
-        private String size;
-        private String pizzaChain;
-
-        public List<String> getSelectedToppings() {
-            return selectedToppings;
+    public void eat() {
+        System.out.println("Pizza is from:" + pizzaChain);
+        System.out.println("Size is " + size);
+        System.out.print("Selected toppings: ");
+        for (String topping : selectedToppings) {
+            System.out.print(topping + " ");
         }
-
-        public void setSelectedToppings(List<String> selectedToppings) {
-            this.selectedToppings = selectedToppings;
-        }
-
-        public String getSize() {
-            return size;
-        }
-
-        public void setSize(String size) {
-            this.size = size;
-        }
-
-        public String getPizzaChain() {
-            return pizzaChain;
-        }
-
-        public void setPizzaChain(String pizzaChain) {
-            this.pizzaChain = pizzaChain;
-        }
-
-        public void addTopping(String topping) {
-            if (ALL_TOPPINGS.contains(topping) && !selectedToppings.contains(topping)) {
-                selectedToppings.add(topping);
-            }
-        }
-
-
-
-        public Pizza createPizza() {
-            return new Pizza(selectedToppings);
-        }
-
+        System.out.println();
     }
+
 }
